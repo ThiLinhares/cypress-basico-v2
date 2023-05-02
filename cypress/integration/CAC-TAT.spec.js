3ebb4ba4088b98f2acf0b825e936dd6bb080875e
ThiLinhares <thiagozudo@hotmail.com>
2023-05-02 18:48:53 -0300

    Revisão do curso

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -179,11 +179,54 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.contains('Talking About Testing').should('be.visible')
     })
 
-    it('', function(){
-        
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+      })
+
+    it('preenche a area de texto usando o comando invoke', function(){
+        const longText = Cypress._.repeat('0123456789', 20)
+
+        cy.get('#open-text-area')
+            .invoke('val', longText)
+            .should('have.value', longText)
     })
 
+    it('faz uma requisição HTTP', function(){
+        cy.request({
+            method: 'GET',
+            url: 'https://cac-tat.s3.eu-central-1.amazonaws.com/index.html'
+          }).should(function(response){
+            const {status, statusText, body} = response //desestruturando  o responde body da pagina 
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+          
+          })
+        })
 
+
+    it.only('Encontra o gato escondido', function(){
+            cy.get('#cat')
+              .invoke('show')
+              .should('be.visible')
+            cy.get('#title')
+              .invoke('text', 'CAT TAT')
+            cy.get('#subtitle')
+              .invoke('text', 'I love <3 cats')
+        })
 
 
 })
